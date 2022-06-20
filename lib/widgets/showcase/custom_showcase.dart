f1d9b82e2784945175f7bd4df151a729453477ef
PaoloTolentino <paolo.tolentino@ciit.edu.ph>
2022-06-20 10:40:28 +0800

    added customized showcase widget based on theme

---
+++ lib/widgets/showcase/custom_showcase.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:showcaseview/showcaseview.dart';
+
+class CustomShowCase extends StatelessWidget {
+  const CustomShowCase({
+    super.key,
+    required this.showCaseKey,
+    required this.child,
+    required this.description,
+  });
+
+  final GlobalKey showCaseKey;
+
+  final Widget child;
+  final String description;
+  @override
+  Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+    return Showcase(
+      key: showCaseKey,
+      description: description,
+      showcaseBackgroundColor: colorScheme.surfaceVariant,
+      descTextStyle: GoogleFonts.inter(
+        color: colorScheme.onSurfaceVariant,
+      ),
+      radius: BorderRadius.circular(12),
+      child: child,
+    );
+  }
+}
